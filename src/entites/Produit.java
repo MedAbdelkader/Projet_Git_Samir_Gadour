20155f0b1b546b5b3068c6e8adde85919112c932
SamirAffes <samir.affes@esprit.tn>
2025-01-12 11:44:50 +0100

    hello

--- src/entites/Produit.java
+++ src/entites/Produit.java
@@ -1,7 +1,32 @@
 package entites;
 
 public class Produit {
-    //TODO : AJOUTER LES CONSTRUCTEURS
-    protected String codeProduit;
+    private String nomProduit;
+    private double prix;
+    private String categorie;
 
+    // Constructeur paramétré
+    public Produit(String nomProduit, double prix, String categorie) {
+        this.nomProduit = nomProduit;
+        this.prix = prix;
+        this.categorie = categorie;
+    }
+
+    // Méthode pour afficher les détails du produit
+    public void afficherDetails() {
+        System.out.println("Produit: " + nomProduit + ", Prix: " + prix + ", Catégorie: " + categorie);
+    }
+
+    // Méthode pour appliquer une remise au produit
+    public double appliquerRemise(double pourcentage) {
+        return prix - (prix * pourcentage / 100);
+    }
+
+    // Méthode pour obtenir le prix du produit
+    public double getPrix() {
+        return prix;
+    }
+    public String getNomProduit() {
+        return nomProduit;
+    }
 }
